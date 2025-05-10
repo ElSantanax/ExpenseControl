4aea2e6b1ad637b9be612104f1d3c084d755e7bb
ElSantanax <josesantanaserrano130598@gmail.com>
2025-05-10 01:52:23 -0500

    feat: agregar manejo de errores en el formulario de gastos y componente de mensaje de error

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -5,6 +5,7 @@ import DatePicker from 'react-date-picker';
 import 'react-calendar/dist/Calendar.css';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
+import ErrorMessage from './ErrorMessage';
 
 export default function ExpenseForm() {
 
@@ -14,6 +15,8 @@ export default function ExpenseForm() {
         category: '',
         date: new Date()
     })
+
+    const [error, setError] = useState('')
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
         const { name, value } = e.target
@@ -32,13 +35,26 @@ export default function ExpenseForm() {
         })
     }
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (Object.values(expense).includes('')) {
+            setError('Todos los campos son obligatorios')
+            return
+        }
+        console.log('todo bien...');
+    }
+
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <legend
                 className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
             >
                 Nuevo Gasto
             </legend>
+
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+
             <div className="flex flex-col gap-2">
                 <label
                     htmlFor="expenseName"
