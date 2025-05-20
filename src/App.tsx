831647c2c472f32d5547cfb1f092d9ab732a4b94
ElSantanax <josesantanaserrano130598@gmail.com>
2025-05-19 19:10:10 -0500

    feat: agregar componente ExpenseList para mostrar la lista de gastos en la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTraker from "./components/BudgetTraker"
 import ExpenseModal from "./components/ExpenseModal"
+import ExpenseList from "./components/ExpenseList"
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
             </div>
             {isValidBudget && (
                 <main className="max-w-3xl mx-auto py-10">
+                    <ExpenseList />
                     <ExpenseModal />
                 </main>
             )}
