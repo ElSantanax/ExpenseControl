e61c71a6285e1bab4c964053e621ab49fd1ca76d
ElSantanax <josesantanaserrano130598@gmail.com>
2025-05-22 14:13:38 -0500

    feat: implementar funcionalidad para obtener un gasto por ID y actualizar el estado de edición en el reductor de presupuesto

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -6,18 +6,21 @@ export type BudgetActions =
     { type: 'show-modal' } |
     { type: 'close-modal' } |
     { type: 'add-expense', payload: { expense: DraftExpense } } |
-    { type: 'remove-expense', payload: { id: Expense['id'] } }
+    { type: 'remove-expense', payload: { id: Expense['id'] } } |
+    { type: 'get-expense-by-id', payload: { id: Expense['id'] } }
 
 export type BudgetState = {
     budget: number
     modal: boolean
     expenses: Expense[]
+    editingId: Expense['id']
 }
 
 export const initialState: BudgetState = {
     budget: 0,
     modal: false,
-    expenses: []
+    expenses: [],
+    editingId: ''
 }
 
 const createExpense = (draftExpense: DraftExpense): Expense => {
@@ -71,5 +74,12 @@ export const BudgetReducers = (
         }
     }
 
+    if (action.type === 'get-expense-by-id') {
+        return {
+            ...state,
+            editingId: action.payload.id,
+            modal: true
+        }
+    }
     return state
 }
