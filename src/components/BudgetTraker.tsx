9ee049736c2ef5c9809f32aa3445cd586ce1ccc2
ElSantanax <josesantanaserrano130598@gmail.com>
2025-05-23 18:35:20 -0500

    feat: actualizar el componente BudgetTraker para calcular y mostrar el presupuesto, disponible y gastado dinámicamente

--- src/components/BudgetTraker.tsx
+++ src/components/BudgetTraker.tsx
@@ -1,6 +1,13 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTraker() {
+
+    const { state } = useBudget()
+    const totalExpense = useMemo(() => state.expenses.reduce((total, expense) => expense.amount + total, 0), [state.expenses])
+    const remainingBudget = state.budget - totalExpense
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex justify-center">
@@ -11,17 +18,17 @@ export default function BudgetTraker() {
 
                 <AmountDisplay
                     label='Presupuesto'
-                    amount={300}
+                    amount={state.budget}
                 />
 
                 <AmountDisplay
                     label='Disponible'
-                    amount={200}
+                    amount={remainingBudget}
                 />
 
                 <AmountDisplay
                     label='Gastado'
-                    amount={100}
+                    amount={totalExpense}
                 />
 
                 <button
